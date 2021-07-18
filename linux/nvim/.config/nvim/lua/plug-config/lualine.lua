b87a966019db8b3d9e018075dc36c281f7947976
Markus Nyman <markus@markusn.dev>
2021-07-18 21:05:19 +0300

    Statusline fixes

--- linux/nvim/.config/nvim/lua/plug-config/lualine.lua
+++ linux/nvim/.config/nvim/lua/plug-config/lualine.lua
@@ -1,3 +1,33 @@
+local get_lsp_client = function(msg)
+  msg = msg or "LSP Inactive"
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  local lsps = ""
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      -- print(client.name)
+      if lsps == "" then
+        -- print("first", lsps)
+        lsps = client.name
+      else
+        if not string.find(lsps, client.name) then
+          lsps = lsps .. ", " .. client.name
+        end
+        -- print("more", lsps)
+      end
+    end
+  end
+  if lsps == "" then
+    return msg
+  else
+    return lsps
+  end
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -8,12 +38,13 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
+    lualine_b = {'branch', 'diff'},
     lualine_c = {'filename'},
     lualine_x = {
       { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
       'encoding',
-      'filetype'
+      'filetype',
+      get_lsp_client
     },
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -26,7 +57,5 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
-  extensions = {'fugitive'}
 }
 
