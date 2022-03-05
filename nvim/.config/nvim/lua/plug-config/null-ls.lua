3b8fa6c6aab2017cc7968fb12cf31f225101b8bd
Markus Nyman <nymak@tuta.io>
2022-03-05 21:55:04 +0200

    Remove comments

--- nvim/.config/nvim/lua/plug-config/null-ls.lua
+++ nvim/.config/nvim/lua/plug-config/null-ls.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require("lspconfig")
 local null_ls = require("null-ls")
 local lsp_helpers = require('lsp.lsp-helpers')
 
@@ -8,6 +7,5 @@ null_ls.setup({
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.prettier,
   },
-  on_attach = lsp_helpers.on_attach
-  -- TODO: Add should_attach that checks if eslintrc or prettierc exists and only attaches then
+  on_attach = lsp_helpers.on_attach,
 })
