de9c6344953483c1bf8a66b3d9031fb6842fcaf0
Markus Nyman <nymak@tuta.io>
2022-03-05 21:44:14 +0200

    Remove active LSPs from lualine

--- nvim/.config/nvim/lua/plug-config/lualine.lua
+++ nvim/.config/nvim/lua/plug-config/lualine.lua
@@ -1,33 +1,3 @@
-local get_lsp_client = function(msg)
-  msg = msg or "LSP Inactive"
-  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-  local clients = vim.lsp.get_active_clients()
-  if next(clients) == nil then
-    return msg
-  end
-  local lsps = ""
-  for _, client in ipairs(clients) do
-    local filetypes = client.config.filetypes
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-      -- print(client.name)
-      if lsps == "" then
-        -- print("first", lsps)
-        lsps = client.name
-      else
-        if not string.find(lsps, client.name) then
-          lsps = lsps .. ", " .. client.name
-        end
-        -- print("more", lsps)
-      end
-    end
-  end
-  if lsps == "" then
-    return msg
-  else
-    return lsps
-  end
-end
-
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -44,7 +14,6 @@ require('lualine').setup {
       { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
       'encoding',
       'filetype',
-      get_lsp_client
     },
     lualine_y = {'progress'},
     lualine_z = {'location'}
