6a5df566bd6d2e6677543101de597b41fcce605a
Markus Nyman <nymak@tuta.io>
2022-03-05 23:39:11 +0200

    Change formatter to black

--- nvim/.config/nvim/lua/plug-config/null-ls.lua
+++ nvim/.config/nvim/lua/plug-config/null-ls.lua
@@ -6,7 +6,7 @@ null_ls.setup({
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.autopep8,
+    null_ls.builtins.formatting.black,
   },
   on_attach = lsp_helpers.on_attach,
 })
