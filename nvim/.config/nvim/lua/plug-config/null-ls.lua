07bcc791d3a4cfa519e00052a95c269fe0384252
Markus Nyman <nymak@tuta.io>
2022-03-08 03:31:57 +0200

    Add condition for eslint and prettier

--- nvim/.config/nvim/lua/plug-config/null-ls.lua
+++ nvim/.config/nvim/lua/plug-config/null-ls.lua
@@ -1,11 +1,23 @@
 local null_ls = require("null-ls")
 local lsp_helpers = require('lsp.lsp-helpers')
 
+function eslint_condition(utils)
+  return utils.root_has_file({ ".eslintrc", ".eslintrc.json", ".eslintrc.js" })
+end
+
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.eslint_d.with{
+      condition = eslint_condition
+    },
+    null_ls.builtins.code_actions.eslint_d.with{
+      condition = eslint_condition
+    },
+    null_ls.builtins.formatting.prettier.with{
+      condition = function (utils)
+        return utils.root_has_file({ ".prettierrc", ".prettierrc.json", ".prettierrc.js" })
+      end
+    },
     null_ls.builtins.formatting.black,
   },
   on_attach = lsp_helpers.on_attach,
