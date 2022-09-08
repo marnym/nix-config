4c2315eaf403ff8a9f0220b4a0a77a303162e9b2
Markus Nyman <nymak@tuta.io>
2022-09-08 18:35:04 +0300

    add options for performance

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,7 @@
 local wezterm = require("wezterm")
 return {
 	color_scheme = "tokyonight",
+	-- Some options that should reduce CPU usage
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
 }
