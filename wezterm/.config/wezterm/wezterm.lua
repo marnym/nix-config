be7aee4ab96a68c3007671665c8de7ce16c54128
Markus Nyman <markus.nyman@futurice.com>
2022-11-23 11:00:51 +0200

    remove wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require("wezterm")
-return {
-	color_scheme = "tokyonight",
-	-- Some options that should reduce CPU usage
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-}
