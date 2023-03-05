8fe7c8835566f9336204b83beb1d7376feab3d13
Markus Nyman <markus@nyman.dev>
2023-03-05 13:39:16 +0200

    Set font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,3 +1,10 @@
+local wezterm = require "wezterm"
+
 return {
+    font = wezterm.font_with_fallback {
+        'SF Mono',
+        'JetBrainsMono Nerd Font',
+    },
+    font_size = 13.0,
     color_scheme = "Catppuccin Mocha",
 }
