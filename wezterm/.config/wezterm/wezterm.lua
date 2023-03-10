098e1e2e6e2731bfc1e61e109b9af6fbb3ab766e
Markus Nyman <markus@nyman.dev>
2023-03-11 00:04:27 +0200

    change setting font function

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 local wezterm = require "wezterm"
 
 return {
-    font = wezterm.font_with_fallback {
-        'JetBrainsMono Nerd Font',
-    },
+    font = wezterm.font 'JetBrainsMono Nerd Font',
     font_size = 14.0,
     color_scheme = "Catppuccin Mocha",
 }
