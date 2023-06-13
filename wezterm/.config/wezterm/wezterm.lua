8699de09ee01af350d9c2fac40e4403b61afc371
Markus Nyman <markus@nyman.dev>
2023-06-13 20:46:01 +0300

    hide wezterm tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require "wezterm"
-local act = wezterm.action
 
 return {
     font = wezterm.font 'BlexMono Nerd Font',
     font_size = 14.0,
     color_scheme = "Catppuccin Mocha",
+    enable_tab_bar = false,
 }
