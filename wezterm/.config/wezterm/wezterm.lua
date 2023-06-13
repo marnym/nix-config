3c2d785ac6551ae67ef2a2aec85d2c71a61818ea
Markus Nyman <markus@nyman.dev>
2023-06-13 14:08:40 +0300

    Set font to IBM Plex Mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require "wezterm"
 local act = wezterm.action
 
 return {
-    font = wezterm.font 'JetBrainsMono Nerd Font',
+    font = wezterm.font 'BlexMono Nerd Font Mono',
     font_size = 14.0,
     color_scheme = "Catppuccin Mocha",
 }
