cbf7fb5e2bd2959180f174a48835aae16cc5f844
Markus Nyman <markus@nyman.dev>
2023-06-13 20:37:41 +0300

    Use font version with larger icons

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require "wezterm"
 local act = wezterm.action
 
 return {
-    font = wezterm.font 'BlexMono Nerd Font Mono',
+    font = wezterm.font 'BlexMono Nerd Font',
     font_size = 14.0,
     color_scheme = "Catppuccin Mocha",
 }
