b1b536f178555665dfbc651bbdbf71104d540df5
Markus Nyman <markus@nyman.dev>
2023-06-14 21:03:47 +0300

    move then keyword to same line as if

--- scripts/.local/bin/project.sh
+++ scripts/.local/bin/project.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 selected=$({ find ~/dev/* -mindepth 1 -maxdepth 1 -type d; cat "$HOME/.local/share/nvim/project_nvim/project_history"; } | fzf)
-if [[ -z $selected ]]
-then
+if [[ -z $selected ]]; then
     exit 0
 fi
 
@@ -10,19 +9,16 @@ selected_name=$(basename "$selected" | tr . _)
 
 tmux_running=$(pgrep tmux)
 
-if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]
-then
+if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
     tmux new-session -s "$selected_name" -c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t="$selected_name" 2> /dev/null
-then
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
     tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-if [[ -z "$TMUX" ]]
-then
+if [[ -z "$TMUX" ]]; then
     tmux attach-session -t="$selected_name"
 else
     tmux switch-client -t="$selected_name"
