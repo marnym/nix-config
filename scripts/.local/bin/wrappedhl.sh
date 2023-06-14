0d56d7029035aad26ce4f80b3349217469ea09cc
Markus Nyman <markus@nyman.dev>
2023-06-14 21:01:37 +0300

    fix wrappedhl script

--- scripts/.local/bin/wrappedhl.sh
+++ scripts/.local/bin/wrappedhl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/markus/ || echo "Error: Could not change directory to /home/markus/" && exit 1
+cd "$HOME" || (echo "Error: Could not change directory to /home/markus/" && exit 1)
 
-env GTK_THEME,Catppuccin-Mocha-Standard-Lavender-Dark
-env _JAVA_AWT_WM_NONREPARENTING,1
-env XCURSOR_SIZE,24
+export GTK_THEME=Catppuccin-Mocha-Standard-Lavender-Dark
+export _JAVA_AWT_WM_NONREPARENTING=1
+export XCURSOR_SIZE=24
 
 exec Hyprland
