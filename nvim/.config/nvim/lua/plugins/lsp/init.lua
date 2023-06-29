adfcef40873fb7618f6abe2d9375ebf0814711d1
Markus Nyman <markus@nyman.dev>
2023-06-29 07:06:57 +0300

    use eslint and prettier when not using deno

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -76,10 +76,21 @@ return {
 			null_ls.setup {
 				debug = false,
 				sources = {
-					-- TODO: Check that deno.json does NOT exist
-					null_ls.builtins.diagnostics.eslint_d,
-					null_ls.builtins.code_actions.eslint_d,
-					null_ls.builtins.formatting.prettierd,
+					null_ls.builtins.diagnostics.eslint_d.with({
+						condition = function(utils)
+							return not utils.root_has_file("deno.json")
+						end,
+					}),
+					null_ls.builtins.code_actions.eslint_d.with({
+						condition = function(utils)
+							return not utils.root_has_file("deno.json")
+						end,
+					}),
+					null_ls.builtins.formatting.prettierd.with({
+						condition = function(utils)
+							return not utils.root_has_file("deno.json")
+						end,
+					}),
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.code_actions.gitsigns,
 					null_ls.builtins.diagnostics.markdownlint.with {
