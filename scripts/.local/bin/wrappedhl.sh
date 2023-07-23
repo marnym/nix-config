49ed0431cec449f33c830b550392e1056187cac6
Markus <markus@nyman.dev>
2023-07-23 15:32:03 +0300

    edit GTK_THEME env variable

--- scripts/.local/bin/wrappedhl.sh
+++ scripts/.local/bin/wrappedhl.sh
@@ -2,8 +2,7 @@
 
 cd "$HOME" || (echo "Error: Could not change directory to /home/markus/" && exit 1)
 
-export GTK_THEME=Catppuccin-Mocha-Standard-Mauve-dark
-export _JAVA_AWT_WM_NONREPARENTING=1
+export GTK_THEME=Qogir-dark
 export XCURSOR_SIZE=24
 
 exec Hyprland
