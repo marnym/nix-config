a7b5276621aeead45c9db6728314bd3006e3d4e3
Markus Nyman <markus@nyman.dev>
2023-08-23 02:12:27 +0300

    change font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,23 +8,17 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+config.font = wezterm.font_with_fallback({
+    "JetBrains Mono Nerd Font",
+    "JetBrains Mono",
+})
+
 if success and stdout:match('Darwin') then
     wezterm.log_info("Running on macOS")
-    config.font = wezterm.font_with_fallback({
-        "SF Mono",
-        "JetBrains Mono Nerd Font",
-        "JetBrains Mono",
-    })
     config.font_size = 14.0
     config.color_scheme = "tokyonight-storm"
 else
     wezterm.log_info("Running on Linux")
-    config.font = wezterm.font_with_fallback({
-        "IBM Plex Mono",
-        "BlexMono Nerd Font",
-        "JetBrains Mono Nerd Font",
-        "JetBrains Mono",
-    })
     config.font_size = 16.0
     config.color_scheme = "Tokyo Night Storm"
 end
