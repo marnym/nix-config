5e7a652382f22fc4d417bda09db2b00c7998d1e9
Markus Nyman <markus@nyman.dev>
2023-08-25 20:08:27 +0300

    different fonts on macOS and Linux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,17 +8,15 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.font = wezterm.font_with_fallback({
-    "JetBrains Mono Nerd Font",
-    "JetBrains Mono",
-})
 
 if success and stdout:match('Darwin') then
     wezterm.log_info("Running on macOS")
+    config.font = wezterm.font("JetBrainsMono Nerd Font")
     config.font_size = 14.0
     config.color_scheme = "tokyonight-storm"
 else
     wezterm.log_info("Running on Linux")
+    config.font = wezterm.font("JetBrains Mono Nerd Font")
     config.font_size = 16.0
     config.color_scheme = "Tokyo Night Storm"
 end
