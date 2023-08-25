7f8b164913899f2ae3143ba9b03f58b86a096864
Markus Nyman <markus@nyman.dev>
2023-08-25 19:51:31 +0300

    disable rainbow config temporarily

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -38,13 +38,13 @@ function M.config()
 			"yaml",
 		},
 		highlight = {
-			enable = true, -- false will disable the whole extension
-		},
-		rainbow = {
 			enable = true,
-			extended = true,
-			max_file_lines = 1000,
 		},
+		-- rainbow = {
+		-- 	enable = true,
+		-- 	extended = true,
+		-- 	max_file_lines = 1000,
+		-- },
 	}
 end
 
