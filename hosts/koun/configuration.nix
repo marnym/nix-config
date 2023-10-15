f843375431a5de5b66fcfcd62964a179a2f387bc
Markus Nyman <markus@nyman.dev>
2023-10-15 20:41:12 +0300

    add syncthing for koun

--- hosts/koun/configuration.nix
+++ hosts/koun/configuration.nix
@@ -8,8 +8,25 @@
 
   networking.hostName = "koun";
 
-  services.syncthing.devices = {
-    "persC" = { id = "RMIX3JF-FQN5TTD-M62XRKG-SKVKXHJ-7ETUFPN-U4JOEG2-CFLZCSB-F4TXAQ2"; };
-    "macOS" = { id = "TA4TOU5-BFEAXWC-2ZIEN6I-G6PZAND-R2ADBUQ-OIDYGKC-NC5E3E6-ZDYOZAB"; };
+  services.syncthing = {
+    enable = true;
+    user = "markus";
+    dataDir = "/home/markus";
+    configDir = "/home/markus/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    devices = {
+      "persC" = { id = "RMIX3JF-FQN5TTD-M62XRKG-SKVKXHJ-7ETUFPN-U4JOEG2-CFLZCSB-F4TXAQ2"; };
+      "macOS" = { id = "TA4TOU5-BFEAXWC-2ZIEN6I-G6PZAND-R2ADBUQ-OIDYGKC-NC5E3E6-ZDYOZAB"; };
+    };
+    folders = {
+      "Cloud" = {
+        id = "capiu-mnq67";
+        label = "Cloud";
+        path = "/home/markus/Cloud";
+        devices = [ "persC" "macOS" ];
+        type = "sendreceive";
+      };
+    };
   };
 }
