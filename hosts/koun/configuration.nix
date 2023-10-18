81a6940993a867e34146e3b6a9c0ada0cad199f2
Markus Nyman <markus@nyman.dev>
2023-10-19 02:36:32 +0300

    rename persH to pers-h

--- hosts/koun/configuration.nix
+++ hosts/koun/configuration.nix
@@ -9,13 +9,13 @@
   networking.hostName = "koun";
 
   services.syncthing.devices = {
-    "persH" = {
+    "pers-h" = {
       id = "RMIX3JF-FQN5TTD-M62XRKG-SKVKXHJ-7ETUFPN-U4JOEG2-CFLZCSB-F4TXAQ2";
       addresses = [ "tcp://192.168.0.5:22000" ];
     };
     "macOS" = { id = "TA4TOU5-BFEAXWC-2ZIEN6I-G6PZAND-R2ADBUQ-OIDYGKC-NC5E3E6-ZDYOZAB"; };
   };
 
-  services.syncthing.folders.Cloud.devices = [ "persH" "macOS" ];
+  services.syncthing.folders.Cloud.devices = [ "pers-h" "macOS" ];
 
 }
