bee3b889d621fff580630c35b1659a22962c38df
Markus Nyman <markus@nyman.dev>
2023-10-20 07:47:37 +0300

    add packages

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -1,8 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./font.nix
     ./firefox.nix
     ./wezterm
     ./jetbrains
+  ];
+
+  home.packages = with pkgs; [
+    spotify
+    telegram-desktop
+    obsidian
   ];
 }
--- home/features/desktop/common/wayland/default.nix
+++ home/features/desktop/common/wayland/default.nix
@@ -1,9 +1,18 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./gammastep.nix
     ./eww
     ./fuzzel
     ./mako
     ./zathura
+  ];
+
+  home.packages = with pkgs; [
+    xdg-utils
+    xfce.thunar
+    wl-clipboard
+    hyprshot
+    thunderbird-bin
+    pavucontrol
   ];
 }
