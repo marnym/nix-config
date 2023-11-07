20c6f326defb96219016185710f2c5f293cc871e
Markus Nyman <markus@nyman.dev>
2023-11-07 09:02:22 +0200

    run linter conditionally

--- home/features/nvim/config/lua/plugins/lint.lua
+++ home/features/nvim/config/lua/plugins/lint.lua
@@ -11,19 +11,28 @@ return {
                 }
             })
 
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                javascriptreact = { "eslint_d" },
-                typescriptreact = { "eslint_d" },
-                astro = { "eslint_d" },
-            }
+            local eslint_d_exists = vim.fn.executable("eslint_d") == 1
+
+            ---@param path string
+            ---@return boolean
+            local function root_has(path)
+                return vim.fn.glob(path) ~= ""
+            end
 
             vim.api.nvim_create_autocmd({ "BufWritePost" },
                 {
+                    group = vim.api.nvim_create_augroup("lint", { clear = true }),
                     callback = function()
+                        local lint = require("lint")
                         lint.try_lint()
+
+                        if root_has("deno.json") then
+                            lint.try_lint("deno")
+                        elseif root_has(".eslintrc*") and eslint_d_exists then
+                            lint.try_lint("eslint_d")
+                        elseif root_has(".eslintrc*") and not eslint_d_exists then
+                            lint.try_lint("eslint")
+                        end
                     end,
                 })
         end
