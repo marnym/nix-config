eced1d0a5eff4f1eefc772951713d12e0015e72c
Markus Nyman <markus@nyman.dev>
2023-12-11 20:48:26 +0200

    do not verify when closing kitty

--- home/features/desktop/common/kitty/default.nix
+++ home/features/desktop/common/kitty/default.nix
@@ -8,6 +8,9 @@
     };
     shellIntegration.enableFishIntegration = true;
     theme = "Gruvbox Material Dark Hard";
+    extraConfig = ''
+      confirm_os_window_close 0
+    '';
     # darwinLaunchOptions = [
     #   "--single-instance"
     # ];
