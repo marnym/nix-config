0a6a3b0f66135b0e51282dabb476f852a6493da2
Markus Nyman <markus@nyman.dev>
2024-01-04 21:20:58 +0200

    add power management

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -9,12 +9,31 @@
     ../common/global
     ../common/features/docker.nix
     ../common/features/hyprland.nix
-    ../common/features/intel-undervolt.nix
     ../common/features/mullvad.nix
     ../common/features/syncthing.nix
   ];
 
   networking.hostName = "thinkpad";
 
-  environment.systemPackages = [ pkgs.acpi pkgs.powertop ];
+  environment.systemPackages = with pkgs; [ acpi powertop intel-gpu-tools intel-undervolt ];
+
+  services.undervolt = {
+    enable = true;
+    useTimer = true;
+    analogioOffset = 0;
+    coreOffset = -120;
+    uncoreOffset = -50;
+    gpuOffset = -75;
+    p1.limit = 60;
+    p1.window = 30;
+    p2.limit = 30;
+    p2.window = 100000;
+    temp = 80;
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+    powertop.enable = true;
+  };
 }
