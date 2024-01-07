c5d66995fa40258d880fc234b01668696e2fe460
Markus Nyman <markus@nyman.dev>
2024-01-07 06:35:15 +0200

    turn off display 10s after locking

--- home/features/desktop/common/wayland/swayidle.nix
+++ home/features/desktop/common/wayland/swayidle.nix
@@ -9,13 +9,18 @@
     ];
     timeouts = [
       {
-        timeout = 180;
+        timeout = 10;
         command = "if pgrep swaylock; then hyprctl dispatch dpms off; fi";
         resumeCommand = "hyprctl dispatch dpms on";
       }
       {
         timeout = 300;
         command = "${pkgs.swaylock}/bin/swaylock -fF";
+      }
+      {
+        timeout = 310;
+        command = "hyprctl dispatch dpms off";
+        resumeCommand = "hyprctl dispatch dpms on";
       }
       {
         timeout = 600;
