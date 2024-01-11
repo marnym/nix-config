0110ee5b6cbfe0ce58d5d7d03bc4c224fc4eea6b
Markus Nyman <markus@nyman.dev>
2024-01-11 12:15:05 +0200

    use correct path for swaylock and hyprctl

--- home/features/desktop/common/wayland/swayidle.nix
+++ home/features/desktop/common/wayland/swayidle.nix
@@ -1,31 +1,36 @@
 { pkgs, ... }: {
-  services.swayidle = {
-    enable = true;
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.swaylock}/bin/swaylock -fF";
-      }
-    ];
-    timeouts = [
-      {
-        timeout = 10;
-        command = "if pgrep swaylock; then hyprctl dispatch dpms off; fi";
-        resumeCommand = "hyprctl dispatch dpms on";
-      }
-      {
-        timeout = 150;
-        command = "${pkgs.swaylock}/bin/swaylock -fF";
-      }
-      {
-        timeout = 200;
-        command = "hyprctl dispatch dpms off";
-        resumeCommand = "hyprctl dispatch dpms on";
-      }
-      {
-        timeout = 300;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
-    ];
-  };
+  services.swayidle =
+    let
+      swaylock = "${pkgs.swaylock}/bin/swaylock";
+      hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+    in
+    {
+      enable = true;
+      events = [
+        {
+          event = "before-sleep";
+          command = "${swaylock} -fF";
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 10;
+          command = "if pgrep swaylock; then hyprctl dispatch dpms off; fi";
+          resumeCommand = "${hyprctl} dispatch dpms on";
+        }
+        {
+          timeout = 150;
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+        {
+          timeout = 200;
+          command = "hyprctl dispatch dpms off";
+          resumeCommand = "${hyprctl} dispatch dpms on";
+        }
+        {
+          timeout = 300;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+    };
 }
