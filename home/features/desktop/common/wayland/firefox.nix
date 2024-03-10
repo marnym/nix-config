c723f69063a0383cd1b536abdd6c749fdfbb0ff6
Markus Nyman <markus@nyman.dev>
2024-03-10 16:36:10 +0200

    remove Firefox profile config

--- home/features/desktop/common/wayland/firefox.nix
+++ home/features/desktop/common/wayland/firefox.nix
@@ -1,17 +1,6 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ... }: {
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
-
-    profiles.default.id = 0;
-    profiles.default.isDefault = true;
-    profiles.default.settings = {
-      # Disable pocket
-      "extensions.pocket.enable" = false;
-      # Disable firefox reacting to media keys
-      "media.hardwaremediakeys.enabled" = false;
-    };
   };
 }
