c4a39bf91340c3e593f3380b3daae1d1f03a5952
Markus Nyman <markus@nyman.dev>
2024-03-13 01:45:09 +0200

    nvim: do not download missing plugins automatically

--- home/features/nvim/config/init.lua
+++ home/features/nvim/config/init.lua
@@ -17,6 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
     -- would be nice to not hardcode this
     lockfile = vim.fn.expand("$HOME/nix-config/home/features/nvim/lazy-lock.json"),
+    install = {
+        missing = false,
+    },
 })
 
 vim.api.nvim_create_autocmd("User", {
