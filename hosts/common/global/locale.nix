e26ae39b6972bb5d094842215be15b6fd62a561f
Markus Nyman <markus@nyman.dev>
2024-04-03 17:14:31 +0300

    use hardware clock in local time

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -2,7 +2,12 @@
   environment.variables = {
     TZ = "Europe/Helsinki";
   };
-  time.timeZone = "Europe/Helsinki";
+
+  time = {
+    timeZone = "Europe/Helsinki";
+    hardwareClockInLocalTime = true;
+  };
+
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     keyMap = "fi";
