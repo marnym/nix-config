e2795cf979e90e1f8f7747a96fc1b5660f7ba66a
Markus Nyman <markus@nyman.dev>
2024-04-12 01:12:50 +0300

    add typst-watch.nvim

---
+++ home/features/nvim/config/lua/plugins/local.lua
@@ -0,0 +1,16 @@
+return {
+    -- {
+    --     dir = "/home/markus/dev/typst-watch.nvim",
+    --     opts = {
+    --         preview_cmd = "zathura"
+    --     },
+    --     ft = "typst"
+    -- },
+    {
+        "marnym/typst-watch.nvim",
+        opts = {
+            preview_cmd = "zathura",
+        },
+        ft = "typst",
+    },
+}
