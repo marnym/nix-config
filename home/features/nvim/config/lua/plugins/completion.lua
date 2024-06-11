b0cb5ca831bf1160ad96c5f1f4d3581373323ef5
Markus Nyman <markus@nyman.dev>
2024-06-11 12:22:03 +0300

    do not lazy load nvim-cmp

--- home/features/nvim/config/lua/plugins/completion.lua
+++ home/features/nvim/config/lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 return {
     "hrsh7th/nvim-cmp",
-    version = false,
-    event = "InsertEnter",
+    lazy = false,
+    priority = 100,
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
